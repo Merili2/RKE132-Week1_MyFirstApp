dfb6cb3c7eeb60cfb20c1a2d577ce64cf71e68aa
Merili Mikku <merilimikku@Merilis-MacBook-Air.local>
2023-09-05 11:48:44 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name: "); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
